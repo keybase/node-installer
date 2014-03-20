de1e915c1fe983b969851d51e0d62701d60047e6
Maxwell Krohn <themax@gmail.com>
2014-03-20 19:09:51 -0400

    this is the way to test the version # after the install.

--- bin/main.js
+++ bin/main.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
+var semver = require('semver');
+
+if (semver.lt(process.version, "0.10.0")) {
+	console.error("We're sorry; keybase requires Node version v0.10 or greater; please upgrade");
+	process.exit(2);
+}
+
 require("../lib/main").run();
